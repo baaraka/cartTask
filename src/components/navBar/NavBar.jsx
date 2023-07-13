ba403b3a419c404b6854e490b6877e9f85e7b403
barakaganai <barakaganai01@gmail.com>
2023-07-13 12:31:49 +0300

    get all items in the cart

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 function NavBar() {
+  const { cartItems } = useContext(CartContext);
+
+  const totalCount = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.quantity;
+    });
+    return total;
+  };
+
   return (
     <div className="navBar">
       <div className="navBarContainer">
@@ -10,6 +22,7 @@ function NavBar() {
         </Link>
         <Link className="link" to="/cart">
           <span>Cart</span>
+          <span style={{ fontSize: "20px" }}>{totalCount()}</span>
         </Link>
       </div>
     </div>
